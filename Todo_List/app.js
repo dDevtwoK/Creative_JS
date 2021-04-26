0d6e67e348303ce85bd779f007d5f6787108bde1
duongbui123 <buiducduong2342000@gmail.com>
2021-04-26 23:41:37 +0700

    finish DOM

--- Todo_List/app.js
+++ Todo_List/app.js
@@ -38,9 +38,31 @@ const deleteCheck = e => {
 };
 
 const filterTodo = e => {
-  const todos = todoList.childNodes;
-  console.log(todos);
-  console.log(e.target.value);
+  const todos = todoList.children;
+  console.log(typeof todos);
+  [...todos].forEach(todo => {
+    switch (e.target.value) {
+      case 'all':
+        todo.style.display = 'flex';
+        break;
+      case 'completed':
+        if (todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+        break;
+      case 'uncompleted':
+        if (!todo.classList.contains('completed')) {
+          todo.style.display = 'flex';
+        } else {
+          todo.style.display = 'none';
+        }
+        break;
+      default:
+        break;
+    }
+  });
 };
 
 // Event Listeners
