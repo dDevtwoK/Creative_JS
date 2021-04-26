9d5ddf8457317ff32e808dee9c402dc6642ea553
duongbui123 <buiducduong2342000@gmail.com>
2021-04-26 23:18:18 +0700

    first commit

---
+++ Todo_List/app.js
@@ -0,0 +1,49 @@
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+const todoContainer = document.querySelector('.todo-container');
+const filterOption = document.querySelector('.filter-todo');
+
+//Functions
+const addTodo = e => {
+  e.preventDefault();
+  const html = `
+        <div class="todo">
+            <li class="todo-item">${todoInput.value}</li>
+            <button class="btn btn-complete"><i class="fas fa-check"></i></button>
+            <button class="btn btn-trash"><i class="fas fa-trash"></i></button>
+        </div>
+    `;
+  todoList.insertAdjacentHTML('afterbegin', html);
+};
+
+const deleteCheck = e => {
+  const item = e.target;
+  if (
+    item.classList.contains('fa-trash') ||
+    item.classList.contains('btn-trash')
+  ) {
+    const todo = item.closest('.todo');
+    todo.classList.add('fail');
+    todo.addEventListener('transitionend', () => {
+      todo.remove();
+    });
+  }
+  if (
+    item.classList.contains('fa-check') ||
+    item.classList.contains('btn-complete')
+  ) {
+    item.closest('.todo').classList.toggle('completed');
+  }
+};
+
+const filterTodo = e => {
+  const todos = todoList.childNodes;
+  console.log(todos);
+  console.log(e.target.value);
+};
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
